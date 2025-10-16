25a563babf4f1fae8e39a4b7c5b20701c7edd247
enwkurs <erinwajca@gmail.com>
2025-10-16 10:05:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oct16ARRAYS
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/enwkurs/oct16ARRAYS)
---
+++ script.js
@@ -0,0 +1,44 @@
+const disneyCharacters = [
+  { type: "mouse", name: "Mickey" },
+  { type: "mouse", name: "Minnie" },
+  { type: "dog", name: "Pluto" },
+  { type: "duck", name: "Donald" },
+  { type: "duck", name: "Daisy" },
+  { type: "dog", name: "Goofy" },
+  { type: "cat", name: "Figaro" },
+  { type: "chipmunk", name: "Chip" },
+  { type: "chipmunk", name: "Dale" },
+  { type: "bird", name: "Clara Cluck" },
+];
+
+//
+//
+
+const food = {
+  vegetables: [
+    { name: "Carrot", color: "orange" },
+    { name: "Broccoli", color: "green" },
+    { name: "Cabbage", color: "green" },
+    { name: "Potato", color: "brown" },
+    { name: "Cucumber", color: "green" },
+  ],
+  fruits: [
+    { name: "Apple", color: "red" },
+    { name: "Banana", color: "yellow" },
+    { name: "Orange", color: "orange" },
+    { name: "Pear", color: "green" },
+    { name: "Mango", color: "orange" },
+  ],
+  berries: [
+    { name: "Strawberry", color: "red" },
+    { name: "Blueberry", color: "blue" },
+    { name: "Raspberry", color: "red" },
+    { name: "Blackberry", color: "black" },
+    { name: "Cloudberry", color: "orange" },
+  ],
+};
+
+//
+//
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];

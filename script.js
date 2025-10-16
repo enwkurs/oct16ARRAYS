3f1bcba2bd9d980c226864c49231169a766e7f8d
enwkurs <erinwajca@gmail.com>
2025-10-16 12:02:38 +0200

    end lesson

--- script.js
+++ script.js
@@ -14,8 +14,17 @@ const disneyCharacters = [
 console.log(disneyCharacters);
 console.log(disneyCharacters[4].name);
 
+// Make the data show on the HTML document
+const list = document.querySelector("#list"); // Point to <ul id="list"> in HTML doc
+
+for ( let index = 0; index < disneyCharacters.length; index++) {
+const listItem = document.createElement("li");
+listItem.textContent = `Name: ${disneyCharacters[index].name}`;
+list.appendChild(listItem);
+};
+
 //
-console.log("---------------------------------------------------------------");
+console.log("-------FOOD-ARRAY---------------------------------------------");
 //
 
 // Object with array (Object nested array)
@@ -42,14 +51,35 @@ const food = {
     { name: "Cloudberry", color: "orange" },
   ],
 };
+console.log("---------------Whole Array---------");
 console.log(food);
-console.log(food.vegetables[2].name);
+
+// console.log(food.vegetables[2].name);
 // To call more you could keep repeatign ".vegetables[2].name" and name each [1] [2] etc, or use "loops"
+
+// Set up a loop that grabs several items at once
+console.log("---------------Check Vegetables---------");
+for ( let index = 0; index < food.vegetables.length; index++) {
+  // why do we start with 0?
+  console.log(food.vegetables[index]);
+}
+
+console.log("-----------Check Foodnames Veggies---------");
+for ( let index = 0; index < food.entries; index++) {
+  const foodNames = food[index];
+  console.log(foodNames.vegetables);
+}
+
+console.log("-----------Check Foodnames Veggies---------");
+for (let index = 0; index < food.vegetables.length; index++) {
+  const foodItem = food.vegetables[index];
+  console.log(foodItem.name);
+}
 
 
 
 //
-console.log("---------------------------------------------------------------");
+console.log("-----FOR-LOOPS-EXAMPLE---------------------------------");
 //
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
